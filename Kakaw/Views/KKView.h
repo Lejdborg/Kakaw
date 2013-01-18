6cad9f4b8767c2d94e1be95ed999ada079edef85
Lejdborg <christoffer.lejdborg@gmail.com>
2013-01-18 09:10:47 +0100

    Set up methods for custom view styling (backgrounds, gradients, rounded corners)

--- Kakaw/Views/KKView.h
+++ Kakaw/Views/KKView.h
@@ -1,14 +1,45 @@
 @interface KKView : NSView
 
-/** Prevent mouse clicks from propagating through the view. */
-@property (readwrite) BOOL preventClickThrough;
+/// ----------------------------------------------------------------------------
+/// @name Layer Styles
+/// ----------------------------------------------------------------------------
+
+/** Background color for filling the view */
+@property (nonatomic, strong) NSColor *backgroundColor;
+
+/** Radius for rounding corners on view */
+@property (nonatomic, readwrite) CGFloat cornerRadius;
+
+/**
+ * Draw a left-right gradient background
+ *
+ * @param colors List of NSColors
+ */
+- (void)drawHorizontalGradientWithColors:(NSArray *)colors;
 
 /**
  * Draw a top-down gradient background
  *
- * @param startColor
- * @param endColor
+ * @param colors List of NSColors
  */
-- (void)setGradientStartColor:(NSColor *)startColor endColor:(NSColor *)endColor;
+- (void)drawVerticalGradientWithColors:(NSArray *)colors;
+
+/**
+ * Draw a gradient at a custom angle by defining the endpoints
+ *
+ * @param colors List of NSColors
+ * @param startPoint Point to start drawing from
+ * @param endPoint Point to end drawing at
+ */
+- (void)drawGradientWithColors:(NSArray *)colors
+                    startPoint:(CGPoint)startPoint
+                      endPoint:(CGPoint)endPoint;
+
+/// ----------------------------------------------------------------------------
+/// @name Mouse Events
+/// ----------------------------------------------------------------------------
+
+/** Prevent mouse clicks from propagating through the view. */
+@property (nonatomic, readwrite) BOOL preventClickThrough;
 
 @end
--- Kakaw/Views/KKView.m
+++ Kakaw/Views/KKView.m
@@ -1,36 +1,63 @@
 #import "KKView.h"
 
 @interface KKView ()
-@property (nonatomic, strong) NSColor *gradientStartColor;
-@property (nonatomic, strong) NSColor *gradientEndColor;
+
+@property (nonatomic, strong) NSArray *gradientColors;
+@property (nonatomic, readwrite) CGPoint gradientStartPoint;
+@property (nonatomic, readwrite) CGPoint gradientEndPoint;
+
 @end
 
 @implementation KKView
 
 // -----------------------------------------------------------------------------
-#pragma mark - GRADIENTS
+#pragma mark - VIEW LIFECYCLE
 // -----------------------------------------------------------------------------
 
-- (void)setGradientStartColor:(NSColor *)startColor endColor:(NSColor *)endColor {
-    [self setGradientStartColor:startColor];
-    [self setGradientEndColor:endColor];
+- (id)initWithFrame:(NSRect)frame {
+	if (self = [super initWithFrame:frame]) {
+        [self setWantsLayer:YES];
+        [self setLayerContentsPlacement:NSViewLayerContentsPlacementScaleAxesIndependently];
+        [self setLayerContentsRedrawPolicy:NSViewLayerContentsRedrawNever];
+    }
 
-    [self needsToDrawRect:self.bounds];
+	return self;
 }
 
 // -----------------------------------------------------------------------------
-#pragma mark - VIEW DRAWING
+#pragma mark - LAYER STYLES
 // -----------------------------------------------------------------------------
 
-- (void)drawRect:(NSRect)dirtyRect {
-    if (self.gradientStartColor && self.gradientEndColor) {
-        NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:self.gradientStartColor
-                                                             endingColor:self.gradientEndColor];
-        [gradient drawInRect:self.bounds angle:270];
-    }
-    else {
-        [super drawRect:dirtyRect];
-    }
+- (void)setBackgroundColor:(NSColor *)backgroundColor {
+    _backgroundColor = backgroundColor;
+    [self applyLayerProperties];
+}
+
+- (void)setCornerRadius:(CGFloat)cornerRadius {
+    _cornerRadius = cornerRadius;
+    [self applyLayerProperties];
+}
+
+- (void)drawHorizontalGradientWithColors:(NSArray *)colors {
+    [self drawGradientWithColors:colors
+                      startPoint:CGPointMake(0.0, 0.5)
+                        endPoint:CGPointMake(1.0, 0.5)];
+}
+
+- (void)drawVerticalGradientWithColors:(NSArray *)colors {
+    [self drawGradientWithColors:colors
+                      startPoint:CGPointMake(0.5, 1.0)
+                        endPoint:CGPointMake(0.5, 0.0)];
+}
+
+- (void)drawGradientWithColors:(NSArray *)colors
+                    startPoint:(CGPoint)startPoint
+                      endPoint:(CGPoint)endPoint {
+    [self setGradientColors:colors];
+    [self setGradientStartPoint:startPoint];
+    [self setGradientEndPoint:endPoint];
+
+    [self applyLayerProperties];
 }
 
 // -----------------------------------------------------------------------------
@@ -41,6 +68,71 @@
     if (!self.preventClickThrough) {
         [super mouseDown:theEvent];
     }
+}
+
+// -----------------------------------------------------------------------------
+#pragma mark - LAYOUT
+// -----------------------------------------------------------------------------
+
++ (BOOL)requiresConstraintBasedLayout {
+	return YES;
+}
+
+- (void)layout {
+	[super layout];
+}
+
+// -----------------------------------------------------------------------------
+#pragma mark - VIEW HIERARCHY
+// -----------------------------------------------------------------------------
+
+- (void)viewDidMoveToSuperview {
+	[self applyLayerProperties];
+}
+
+- (void)viewDidMoveToWindow {
+	[self applyLayerProperties];
+}
+
+// -----------------------------------------------------------------------------
+#pragma mark - LAYER MANAGEMENT
+// -----------------------------------------------------------------------------
+
+- (void)applyLayerProperties {
+    // Clear sublayers
+    for (CALayer *layer in self.layer.sublayers) {
+        [layer removeFromSuperlayer];
+    }
+
+    // Apply layer styles
+    [self.layer setBackgroundColor:self.backgroundColor.CGColor];
+	[self.layer setCornerRadius:self.cornerRadius];
+
+    // Draw gradients
+    if (self.gradientColors) {
+        CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+
+        NSMutableArray *colors = [NSMutableArray array];
+        for (NSColor *color in self.gradientColors) {
+            [colors addObject:(id)color.CGColor];
+        }
+        [gradientLayer setColors:colors];
+
+        [gradientLayer setBounds:self.layer.bounds];
+        [gradientLayer setPosition:CGPointMake(self.bounds.size.width / 2.0,
+                                               self.bounds.size.height / 2.0)];
+
+        [gradientLayer setStartPoint:self.gradientStartPoint];
+        [gradientLayer setEndPoint:self.gradientEndPoint];
+
+        [gradientLayer setCornerRadius:self.cornerRadius];
+        [self.layer addSublayer:gradientLayer];
+    }
+}
+
+- (void)setLayer:(CALayer *)layer {
+	[super setLayer:layer];
+	[self applyLayerProperties];
 }
 
 @end
